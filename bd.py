64a942337d6d118dd186e20bcc33bdd92817f12a
Cauã Campos <116642309+CauaCamp0s@users.noreply.github.com>
2023-09-20 11:05:06 -0300

    frist commit

---
+++ bd.py
@@ -0,0 +1,62 @@
+Carros = [
+    {
+        'id': 1,
+        'marca': 'Fiat',
+        'modelo': 'Marea',
+        'ano': 1999
+    },
+    {
+        'id': 2,
+        'marca': 'Chevrolet',
+        'modelo': 'Cruze',
+        'ano': 2022
+    },
+    {
+        'id': 3,
+        'marca': 'Ford',
+        'modelo': 'Mustang',
+        'ano': 2021
+    },
+    {
+        'id': 4,
+        'marca': 'Toyota',
+        'modelo': 'Corolla',
+        'ano': 2023
+    },
+    {
+        'id': 5,
+        'marca': 'Volkswagen',
+        'modelo': 'Golf',
+        'ano': 2019
+    },
+    {
+        'id': 6,
+        'marca': 'Honda',
+        'modelo': 'Civic',
+        'ano': 2022
+    },
+    {
+        'id': 7,
+        'marca': 'Nissan',
+        'modelo': 'Sentra',
+        'ano': 2023
+    },
+    {
+        'id': 8,
+        'marca': 'Hyundai',
+        'modelo': 'Elantra',
+        'ano': 2022
+    },
+    {
+        'id': 9,
+        'marca': 'BMW',
+        'modelo': 'X5',
+        'ano': 2023
+    },
+    {
+        'id': 10,
+        'marca': 'Mercedes-Benz',
+        'modelo': 'C-Class',
+        'ano': 2021
+    }
+]
